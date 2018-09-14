8f050a7eac4d3c1bfc1d354eedd63ae5c38b57d0
David Walker <david.walker@mclaren.com>
2018-09-14 12:20:39 +0100

    Refactored AS init, so we can constexpr the starting positions.

--- BattleShapes/attacking_shape.cpp
+++ BattleShapes/attacking_shape.cpp
@@ -1,33 +1,41 @@
 #include "attacking_shape.h"
 #include <cstdlib>
 #include <complex>
+#include <tuple>
 #include "screen.h"
 
 constexpr const auto pi = 3.141592f;
 constexpr const auto RADIANS = pi / 180.0f;
 
+std::tuple<float, float, float> calcualte_starting_pos() {
+	auto angle = (rand() % 10) *
+		36.0f;  // 10 starting points about the circle seems enough.
+	auto theta = angle * RADIANS;
+
+	auto r = gScreen.height / 2.0f;
+	auto cart = std::polar<float>(r, theta);
+
+	auto x = gScreen.center.x + cart.real() - 16.0f;
+	auto y = gScreen.center.y + cart.imag() - 16.0f;
+
+	auto itheta = atan2(gScreen.center.y - 16 - y, gScreen.center.x - 16 - x);
+	if (itheta < 0.0) {
+		itheta += (2 * pi);
+	}
+
+	return std::make_tuple(x, y, itheta);
+}
+
+
 void AttackingShape::init() {
-  auto angle = (rand() % 10) *
-               36.0f;  // 10 starting points about the circle seems enough.
-  auto theta = angle * RADIANS;
-
-  auto r = gScreen.height / 2.0f;
-  auto cart = std::polar<float>(r, theta);
-
-  auto x = gScreen.center.x + cart.real() - 16.0f;
-  auto y = gScreen.center.y + cart.imag() - 16.0f;
-
-  auto itheta = atan2(gScreen.center.y - 16 - y, gScreen.center.x - 16 - x);
-  if (itheta < 0.0) {
-    itheta += (2 * pi);
-  }
-
-  this->pos.x = x;
-  this->pos.y = y;
-  this->theta = itheta;
-  this->rect.x = static_cast<int>(x + 0.5f);
-  this->rect.y = static_cast<int>(y + 0.5f);
-  this->visibility = true;
+	float x, y, itheta;
+	std::tie(x, y, itheta) = calcualte_starting_pos();
+	this->pos.x = x;
+	this->pos.y = y;
+	this->theta = itheta;
+	this->rect.x = static_cast<int>(x + 0.5f);
+	this->rect.y = static_cast<int>(y + 0.5f);
+	this->visibility = true;
 }
 
 
@@ -39,6 +47,7 @@ void AttackingShape::tick() {
 	this->rect.x = static_cast<int>(this->pos.x + 0.5f );
 	this->rect.y = static_cast<int>(this->pos.y + 0.5f);
 }
+
 
 void AttackingShape::kill() {
 	this->visibility = false;
